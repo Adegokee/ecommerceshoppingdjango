be72e6a463b0f90a2dd25d137d93e859853d4254
adegoke <ababatunde022@gmail.com>
2023-01-03 18:21:51 +0100

    first commit

--- order/views.py
+++ order/views.py
@@ -7,6 +7,9 @@ import datetime
 
 # Create your views here.
 
+def payments(request):
+    return render(request, 'orders/payments.html')
+
 def place_order(request, total=0,quantity=0):
    
     current_user = request.user
@@ -42,15 +45,18 @@ def place_order(request, total=0,quantity=0):
             data.tax=   tax 
             data.ip=request.META.get('REMOTE_ADDR')
             data.save()
-            yr=int(datetime.date.today().strftime('%y'))
+            yr=int(datetime.date.today().strftime('%Y'))
             dt=int(datetime.date.today().strftime('%d'))
             mt=int(datetime.date.today().strftime('%m'))
             d=datetime.date(yr,mt,dt)
-            current_date=d.strftime('%y%m%d')
+            current_date=d.strftime('%Y%m%d')
             order_number=current_date + str(data.id)
             data.order_number=order_number
             data.save()
-            return redirect('checkout')
+            
+            order=Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            context={'order': order, 'cart_items':cart_items, 'total':total, 'tax':tax, 'grand_total':grand_total}
+            return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
                     
