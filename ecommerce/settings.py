674644c6851df2ac575ebfe218bb2533a4012add
adegoke <ababatunde022@gmail.com>
2023-01-07 13:24:57 +0100

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
-import os
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,11 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR /'static'
-# STATICFILES_DIR=[
-#     'ecommerce/static'
-# ]
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIR=[
+    'ecommerce/static'
+]
+# STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR /'media'
