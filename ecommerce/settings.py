a029786bce9e162125d9d24c8d44d66001e7c415
adegoke <ababatunde022@gmail.com>
2023-01-07 13:39:40 +0100

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIR=[
-    'ecommerce/static'
+    'static'
 ]
 # STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 
