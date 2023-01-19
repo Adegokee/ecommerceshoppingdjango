151c7279ff3e80a1126ec04de272af31ffc6c37a
adegokee <ababatunde022@gmail.com>
2023-01-19 09:53:26 +0100

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR /'static'
-# STATICFILES_DIR=[
-#     'ecommerce/static'
-# ]
+STATICFILES_DIR=[
+    'ecommerce/static'
+]
 # STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL='/media/'
